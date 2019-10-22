5c30aa9f29eae2031826ac9883f090949a1e6d9f
mvqa <mvquispea@gmail.com>
2019-10-22 09:49:25 -0500

    primer commit del clase2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/styles.scss'
+import Curso from './Curso'
+const App = () =>(
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<fragment>
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" src="https://mott.pe/noticias/wp-content/uploads/2019/03/los-50-paisajes-maravillosos-del-mundo-para-tomar-fotos.jpg" />
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Título del banner</p>
+        <p> Subtítulo del banner</p>
+        <a href="https://www.youtube.com/watch?v=d_4xk1r9hxU" className="button">Botón del banner</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">
+
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+
+
+</div>
+
+</fragment>
+)
+
 
 export default App;
---
+++ src/Curso.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Curso = () => (
+
+<article className="card">
+  <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+    <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-10/Vue-JS.png" alt="Poster del curso" />
+  </div>
+  <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+    <h3 className="t5 s-mb-2 s-center">
+      Programación orientada a objetos con Go
+    </h3>
+    <div className="s-mb-2 s-main-center">
+      <div className="card__teacher s-cross-center">
+        <div className="card__avatar s-mr-1">
+          <div className="circle img-container">
+            <img src="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-10/josh.png" alt="Tutor" />
+          </div>
+        </div>
+        <span className="small">Alexys Lozada</span>
+      </div>
+    </div>
+    <div className="s-main-center">
+      <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+    </div>
+  </div>
+</article>
+
+)
+export default Curso;
